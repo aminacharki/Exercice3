87814af33d2ed610cd1fe87077907a5b500af573
aminacharki <116524444+aminacharki@users.noreply.github.com>
2022-11-11 01:03:13 +0100

    Update Program.cs

--- Exercice3/Program.cs
+++ Exercice3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,36 @@ namespace Exercice3
     {
         static void Main(string[] args)
         {
+            Point A = new Point(1, 3);
+            Point B = new Point(1, 3);
         }
-    }
-}
+        public  class Point
+        {
+            int abscisse;
+            int ordonné;
+
+            public  Point (int x,int y)
+            {
+                abscisse = x;
+                ordonné = y;
+            }
+            public void traslet(int dx , int dy)
+            {
+                abscisse += dx;
+                ordonné += dy;
+            }
+            public double diseorigine( )
+
+            {
+                return Math.Sqrt(abscisse * abscisse + ordonné * ordonné);
+
+            }
+            public double distanceautre(Point B)
+            {
+                return Math.Sqrt((abscisse - B.abscisse )* (abscisse - B.abscisse)+(ordonné - B.ordonné)*(ordonné - B.ordonné) );
+            }
+
+
+        }
+
+  
